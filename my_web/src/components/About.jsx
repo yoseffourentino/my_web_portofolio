8c41a3b963cf355933e80df19cb17400251728d5
Yosef Fourentino <yoseffourentino30@gmail.com>
2024-10-21 13:32:46 +0700

    .

--- my_web/src/components/About.jsx
+++ my_web/src/components/About.jsx
@@ -1,4 +1,5 @@
 import ocep from "../assets/ocep.png"
+import yosefcv from "../file/YosefCV.pdf"
 
 export default function About(){
     return(
@@ -11,7 +12,7 @@ export default function About(){
                 <h1 className="text-[2.5rem] text-[#d0f6ff] font-bold">About Me</h1>
                 <p className="text-[#d0f6ff] text-justify">Hey everyone, I'm an active student at Multimedia Nusantara University, majoring in Informatics. My skills in IT are mainly in frontend development. I really enjoy creating websites with my wild imagination. I've completed many projects that you can check out on my portfolio page below.</p>
                 <p className="text-[#d0f6ff]"> I'm a cheerful person who loves to chat. One of my favorite ways to unwind when I'm tired is to hang out at cafes with my close friends.</p>
-                <button className="bg-[#d0f6ff] w-[200px] p-[25px] rounded-[10px] font-bold">Download CV</button>
+                <a className="bg-[#d0f6ff] w-[200px] p-[25px] rounded-[10px] font-bold text-center hover:opacity-75" href="https://drive.google.com/file/d/1ytdXKN7Fve4URim93RREiH94MQM0usIy/view?usp=sharing" target="_blank">Download CV</a>
             </div>
         </div>
         </div>
--- my_web/src/components/Portofolio.jsx
+++ my_web/src/components/Portofolio.jsx
@@ -1,13 +1,13 @@
 import Slider from "react-slick";
-import data from "../Data.json";
 import { Link } from 'react-router-dom';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import porto1 from "../assets/porto-1.png"
 
 export default function Portofolio(){
     const settings = {
         dots: true,
-        infinite: true,
+        infinite: false,
         speed: 500,
         slidesToShow: 3,
         slidesToScroll: 1
@@ -16,24 +16,40 @@ export default function Portofolio(){
         <div className="bg-[#7FA1C3]">
             <h1 className="text-[2.5rem] font-bold text-[#F5EDED] pt-[30px] text-center">My Recent Project</h1>
             <div className="max-w-[70%] m-auto p-20">
-            <Slider {...settings}>
-                {
-                    data.map(data => {
-                        return(
-                        <div key={data.id} className="flex flex-col justify-center items-center bg-white text-center rounded-[10px]">
+                <div className="z-0"> 
+                    <Slider {...settings}> 
+                        <div className="flex flex-col justify-center items-center bg-white text-center rounded-[10px]">
                             <div className="bg-[#201E43] rounded-t-[10px]">
-                                <img src={data.img} alt="" className="p-6" />
+                                <img src={porto1} alt="" className="p-6" />
                             </div>
                             <div className="pb-10 flex flex-col gap-5">
-                                <p className="mt-5">{data.title}</p>
-                                <p>{data.desc}</p>
-                                <Link to={data.link} className="bg-[#5090eb] px-5 py-2 rounded-[10px] hover:bg-[#3d71ba] duration-[0.3s] m-auto">See Website</Link>
+                                <p className="mt-5">Portfolio Maker</p>
+                                <p>Sebuah website untuk membuat portofolio kalian menjadi mudah</p>
+                                <Link to="" className="bg-[#5090eb] px-5 py-2 rounded-[10px] hover:bg-[#3d71ba] duration-[0.3s] m-auto">See Website</Link>
                             </div>
                         </div>
-                        )
-                    })
-                }
-            </Slider>    
+                        <div className="flex flex-col justify-center items-center bg-white text-center rounded-[10px]">
+                            <div className="bg-[#201E43] rounded-t-[10px]">
+                                <img src={porto1} alt="" className="p-6" />
+                            </div>
+                            <div className="pb-10 flex flex-col gap-5">
+                                <p className="mt-5">News Portal</p>
+                                <p>a website that has a lot of popular news</p>
+                                <Link to="" className="bg-[#5090eb] px-5 py-2 rounded-[10px] hover:bg-[#3d71ba] duration-[0.3s] m-auto">See Website</Link>
+                            </div>
+                        </div>
+                        <div className="flex flex-col justify-center items-center bg-white text-center rounded-[10px]">
+                            <div className="bg-[#201E43] rounded-t-[10px]">
+                                <img src={porto1} alt="" className="p-6" />
+                            </div>
+                            <div className="pb-10 flex flex-col gap-5">
+                                <p className="mt-5">Portfolio Maker</p>
+                                <p>Sebuah website untuk membuat portofolio kalian menjadi mudah</p>
+                                <Link to="" className="bg-[#5090eb] px-5 py-2 rounded-[10px] hover:bg-[#3d71ba] duration-[0.3s] m-auto">See Website</Link>
+                            </div>
+                        </div>
+                    </Slider>    
+                </div>              
             </div>
         </div>
     )
